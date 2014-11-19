dcc7ca9415f586fd641f7a1d718b7a7c2b2ccfc8
willcai1984 <willcai1984@gmail.com>
2014-11-19 23:00:04 +0800

    Update the scripts

--- __init__.py
+++ __init__.py
@@ -7,73 +7,7 @@ try:
 except ImportError, e:
     raise ImportError (str(e) + """A critical module was not found. Probably this operating system does not support it.""")
 
-from unit import sleep, debug, info, warn, error, str2list
-
-class ExpectArgs(object):
-    def __init__(self):
-        self.parser = argparse.ArgumentParser(description='Login target and execute cmds')
-
-        self.parse.add_argument('-m', '--mode', required=False, default='ssh' , choices=['ssh', 'telnet'], dest='mode',
-                            help='Login mode')
-        
-        self.parse.add_argument('-i', '--ip', required=True, default=None, dest='ip',
-                            help='Target IP')
-
-        self.parse.add_argument('--port', required=False, default=22, type=int, dest='port',
-                            help='Taget port')
-
-        self.parse.add_argument('-u', '--user', required=False, default='admin', dest='user',
-                            help='Login Name')
-        
-        self.parse.add_argument('-p', '--passwd', required=False, default='aerohive', dest='passwd',
-                            help='Login Password')
-
-        self.parse.add_argument('--prompt', required=False, default='AH.*#', dest='prompt',
-                            help='The login prompt you want to meet')
-        
-        self.parse.add_argument('-t', '--timeout', required=False, default=10, type=int, dest='timeout',
-                            help='Time out value for every execute cli step')
-        
-        self.parse.add_argument('-l', '--logfile', required=False, default='.', dest='log_file',
-                            help='The log file path')
-        
-        self.parse.add_argument('-c', '--command', required=False, action='append', default=[], dest='cli_list',
-                            help='The command you want to execute')
-
-        self.parse.add_argument('-f', '--file', required=False, default=False, dest='config_file',
-                            help='The path of configurefile')
-
-        self.parse.add_argument('-w', '--wait', required=False, default=0, type=int, dest='wait',
-                            help='wait time between the current cli and next cli')
-
-        self.parse.add_argument('-r', '--retry', required=False, default=5, type=int, dest='retry',
-                            help='How many times you want to retry when the login step is failed')
-
-        self.parse.add_argument('-sp', '--shellpasswd', required=False, default='', dest='sp',
-                            help='Shell password for enter to shell mode')
-
-        self.parse.add_argument('--debug', required=False, default='error', choices=['debug', 'info', 'warn', 'error'], dest='debug_level',
-                            help='Debug mode, info>warn>error')
-
-        self._parse_args()
-        
-    def _parse_args(self):
-        self.args = self.parser.parse_args()
-        self.mode = self.args.mode
-        self.ip = self.args.ip
-        self.port = self.args.port
-        self.user = self.args.user
-        self.passwd = self.args.passwd
-        self.prompt = self.args.prompt
-        self.timeout = self.args.timeout
-        self.log_file = self.args.log_file
-        self.cli_list = self.args.cli_list
-        self.config_file = self.args.config_file
-        self.wait = self.args.wait
-        self.retry = self.args.retry
-        self.sp = self.args.sp
-        self.debug_level = self.args.debug_level
-
+from unit import sleep, debug, info, warn, error, str2list, ExpectArgs
 
 class ExpectConnect(object):
     def __init__(self):
@@ -92,15 +26,9 @@ class ExpectConnect(object):
         self.retry = self.args.retry
         self.sp = self.args.sp
         self.debug_level = self.args.debug_level
-        self.spawn=None
-        self.is_user=False
-        self.is_passwd=False
-        self.is_prompt=False
-        self.is_no=False
-        self.is_error=False
-        
-        
+        self.child = None
         self._debug()
+        self._tag()
 
     def __str__(self):
         s = []
@@ -121,97 +49,151 @@ class ExpectConnect(object):
         s.append('Debug_level  = %s' % self.debug_level)
         return '\n'.join(s)
 
-    def gen_login(self):
+    def _basic_login(self):
         if self.is_user:
-            info('Meet login successfully, send user to confirm login', self.is_info)
-            info('............Step1 send user to confirm login............', self.is_info)            
-            cli_mode_tuple_list = [(user, 'sendline')]
-            expect_list = [pexpect.TIMEOUT, '[Pp]assword.*']
-            timeout = login_timeout
-            # ##only need expect func, sendnone
-            retry_cli_mode_tuple_list = [[('', 'sendnone')]] * login_retry_times
-            general_login_info = spawn_timeout_retry(cli_mode_tuple_list, expect_list, timeout, retry_cli_mode_tuple_list , login_retry_times, general_login_result, self.is_info)
-            general_login_result = general_login_info[0]
-            general_login_index = general_login_info[1]
-            if general_login_index == 0:
-                print 'Send user to confirm login timeout, please confirm the host is alive'
+            info('[USER]Meet Prompt user,send user to confirm login', self.is_info)
+            self._retry_not_expect(self.user, 'sendline', [pexpect.TIMEOUT, '[Pp]assword.*'])
+            if self.exec_index == 0:
                 is_error = True
-                info('''From is_user jump to is_error''', self.is_info)
-            elif general_login_index == 1:                  
+                info('''[USER]Send username cannot get passwd, Timeout''', self.is_info)
+                info('''[USER]From is_user jump to is_error''', self.is_info)
+            elif self.exec_index == 1:                  
                 is_passwd = True
-                info('''From is_user jump to is_passwd process ''', self.is_info)
-        if is_passwd:
-            info('Meet password successfully, send passwd to confirm login', self.is_info)
-            info('............Step2 send password to confirm login............', self.is_info)
-            cli_mode_tuple_list = [(passwd, 'sendline')]
-            expect_list = [pexpect.TIMEOUT, '\nlogin.*', '[Pp]assword.*', 'yes\|no>:.*', prompt]
-            timeout = login_timeout
-            # ##only need expect func, sendnone
-            retry_cli_mode_tuple_list = [[('', 'sendnone')]] * login_retry_times
-            general_login_info = spawn_timeout_retry(cli_mode_tuple_list, expect_list, timeout, retry_cli_mode_tuple_list , login_retry_times, general_login_result, self.is_info)
-            general_login_result = general_login_info[0]
-            general_login_index = general_login_info[1]
-            if general_login_index == 0:
-                print 'Send password to confirm login timeout, please confirm the host is alive'
+                info('''[USER]From is_user jump to is_passwd''', self.is_info)
+        
+        if self.is_passwd:
+            info('[PASSWD]Meet Prompt passwd,send passwd to confirm login', self.is_info)
+            self._retry_not_expect(self.passwd, 'sendline', [pexpect.TIMEOUT, '\nlogin.*', '[Pp]assword.*', 'yes\|no>:.*', self.prompt])
+            if self.exec_index == 0:
                 is_error = True
-                info('''From is_passwd jump to is_error''', self.is_info)
-            elif general_login_index == 1:
-                print 'Meet login again, user or password maybe incorrect, please check'
+                info('''[PASSWD]Send passwd cannot get expect, Timeout''', self.is_info)
+                info('''[PASSWD]From is_passwd jump to is_error''', self.is_info)
+            elif self.exec_index == 1:
                 is_error = True
-                info('''From is_passwd jump to is_error''', self.is_info)
-            elif general_login_index == 2:
-                print 'Meet password again, password maybe incorrect, please check'
+                info('''[PASSWD]Send passwd, meet user again, may wrong user or passwd''', self.is_info)
+                info('''[PASSWD]From is_passwd jump to is_error''', self.is_info)
+            elif self.exec_index == 2:
                 is_error = True
-                info('''From is_passwd jump to is_error''', self.is_info)                         
-            elif general_login_index == 3:
+                info('''[PASSWD]Send passwd, meet passwd again, may wrong user or passwd''', self.is_info)
+                info('''[PASSWD]From is_passwd jump to is_error''', self.is_info)                         
+            elif self.exec_index == 3:
                 is_no = True
-                info('''From is_passwd jump to is_no process ''', self.is_info)
-            elif general_login_index == 4:
+                info('''[PASSWD]From is_passwd jump to is_no process ''', self.is_info)
+            elif self.exec_index == 4:
                 is_prompt = True
-                info('''From is_passwd jump to is_prompt process ''', self.is_info)
-        if is_no:
-            info('Meet is_default yes or no successfully, send no to not use default config', self.is_info)
-            info('............Step3 send no to not use default config............', self.is_info)
-            cli_mode_tuple_list = [('no', 'sendline')]
-            expect_list = [pexpect.TIMEOUT, prompt]
-            timeout = login_timeout
-            # ##only need expect func, sendnone
-            retry_cli_mode_tuple_list = [[('', 'sendnone')]] * login_retry_times
-            general_login_info = spawn_timeout_retry(cli_mode_tuple_list, expect_list, timeout, retry_cli_mode_tuple_list , login_retry_times, general_login_result, self.is_info)
-            general_login_result = general_login_info[0]
-            general_login_index = general_login_info[1]            
-            if general_login_index == 0:
-                print 'Send no to confirm login timeout, please confirm the host is alive'
+                info('''[PASSWD]From is_passwd jump to is_prompt process ''', self.is_info)
+        
+        if self.is_no:
+            info('[NO]Meet yes or no successfully,send no to not use default config', self.is_info)
+            self._retry_not_expect('no', 'sendline', [pexpect.TIMEOUT, self.prompt])         
+            if self.exec_index == 0:
                 is_error = True
-                info('''From is_no jump to is_error''', self.is_info)
-            elif general_login_index == 1:
+                info('''[NO]Send no cannot get prompt, Timeout''', self.is_info)
+                info('''[NO]From is_no jump to is_error''', self.is_info)
+            elif self.exec_index == 1:
                 is_prompt = True
-                info('''From is_no jump to is_prompt''', self.is_info)
-        if is_prompt:
-            info('Meet prompt successfully, can execute cli now', self.is_info)
-        if is_error:
-            ###v22 del telnet_login_result
-            print 'before is %s, after is %s' % (general_login_result.before, general_login_result.after)
-            general_login_result.close(force=True)
-            return None        
-        return general_login_result
+                info('''[NO]From is_no jump to is_prompt''', self.is_info)
+        
+        if self.is_prompt:
+            info('''[PROMPT]Meet prompt successfully, can execute cli now''', self.is_info)
+        
+        if self.is_error:
+            info('''[ERROR]BEFORE is: %s''' % self.child.before, self.is_info)
+            info('''[ERROR]AFTER is : %s''' % self.child.after, self.is_info)
+            raise ValueError, '''Login Error'''
 
+    def _retry_not_expect(self, cli, mode, exp_list, noexp_index=0, retrymode='enter', retry=5, interval=5):
+        if self.child:
+            info('spawn child exist, send cli directly', self.is_info)
+            exec_cli = '''self.child.%s(cli)''' % mode
+        else:
+            info('spawn child not exist, create spawn firstly', self.is_info)
+            exec_cli = '''self.child=pexpect.spawn(cli)'''
+        exec(exec_cli)
+        self.exec_index = self.child.expect(list(exp_list), interval)
+        if self.exec_index == noexp_index:
+            info('............Trigger Retry Process............', self.is_info)
+            info('CLI         = %s' % cli, self.is_info)
+            info('MODE        = %s' % mode, self.is_info)
+            info('EXPECT_LIST = %s' % str(exp_list), self.is_info)
+            info('RETRY_MODE  = %s' % retrymode, self.is_info)
+            info('RETRY       = %s' % retry, self.is_info)
+            info('INTERVAL    = %s' % interval, self.is_info)
+            for i in range(int(retry)):
+                info('Retry %s time start' % (i + 1), self.is_info)
+                if retrymode == 'enter':
+                    self.child.sendline('')
+                elif retrymode == 'repeat':
+                    exec(exec_cli)
+                self.exec_index = self.child.expect(list(exp_list), interval)
+                if self.exec_index != noexp_index:
+                    info('............End Retry Process............', self.is_info)
+                    return
+                info('Retry %s time end' % (i + 1), self.is_info)
+        else:
+            info('Match expect, no retry', self.is_info)
+            return
+        raise ValueError, '''Retry %s times and still cannot match expect''' % retry
+
+
+    def _retry_not_expect_list(self, cli, mode, exp_list, noexp_index_list=[0], retrymode='enter', retry=5, interval=5):
+        if self.child:
+            info('spawn child exist, send cli directly', self.is_info)
+            exec_cli = '''self.child.%s(cli)''' % mode
+        else:
+            info('spawn child not exist, create spawn firstly', self.is_info)
+            exec_cli = '''self.child=pexpect.spawn(cli)'''
+        exec(exec_cli)
+        self.exec_index = self.child.expect(list(exp_list), interval)
+        noexp_index_list=[int(i) for i in noexp_index_list]
+        if self.exec_index in noexp_index_list:
+            info('............Trigger Retry Process............', self.is_info)
+            info('CLI         = %s' % cli, self.is_info)
+            info('MODE        = %s' % mode, self.is_info)
+            info('EXPECT_LIST = %s' % str(exp_list), self.is_info)
+            info('RETRY_MODE  = %s' % retrymode, self.is_info)
+            info('RETRY       = %s' % retry, self.is_info)
+            info('INTERVAL    = %s' % interval, self.is_info)
+            for i in range(int(retry)):
+                info('Retry %s time start' % (i + 1), self.is_info)
+                if retrymode == 'enter':
+                    self.child.sendline('')
+                elif retrymode == 'repeat':
+                    exec(exec_cli)
+                self.exec_index = self.child.expect(list(exp_list), interval)
+                if self.exec_index not in noexp_index_list:
+                    info('............End Retry Process............', self.is_info)
+                    return
+                info('Retry %s time end' % (i + 1), self.is_info)
+        else:
+            info('Match expect, no retry', self.is_info)
+            return
+        raise ValueError, '''Retry %s times and still cannot match expect''' % retry
+
+     
     def _debug(self):
-        self.is_debug=False
-        self.is_info=False
-        self.is_warn=False
-        self.is_error=False
-        if self.debug_level=='debug':
-            self.is_debug=True
-            self.is_info=True
-            self.is_warn=True
-            self.is_error=True          
-        elif self.debug_level=='info':
-            self.is_info=True
-            self.is_warn=True
-            self.is_error=True   
-        elif self.debug_level=='warn':
-            self.is_warn=True
-            self.is_error=True   
-        elif self.debug_level=='error':
-            self.is_error=True
+        self.is_debug = False
+        self.is_info = False
+        self.is_warn = False
+        self.is_error = False
+        if self.debug_level == 'debug':
+            self.is_debug = True
+            self.is_info = True
+            self.is_warn = True
+            self.is_error = True          
+        elif self.debug_level == 'info':
+            self.is_info = True
+            self.is_warn = True
+            self.is_error = True   
+        elif self.debug_level == 'warn':
+            self.is_warn = True
+            self.is_error = True   
+        elif self.debug_level == 'error':
+            self.is_error = True
+            
+    def _tag(self):
+        self.is_user = False
+        self.is_passwd = False
+        self.is_prompt = False
+        self.is_no = False
+        self.is_error = False
--- unit.py
+++ unit.py
@@ -3,7 +3,7 @@
 # Author: Will
 
 try:
-    import re, time
+    import re, time, argparse, pexpect
 except ImportError, e:
     raise ImportError (str(e) + """A critical module was not found. Probably this operating system does not support it.""")
 
@@ -46,13 +46,82 @@ def str2list(string):
     ran_reg = re.compile('^\d+-\d+$')
     #remove not int para and blank
     i_list = [i.replace(' ', '') for i in p_list if int_reg.search(i)]
-    str_list=[]
+    str_list = []
     for i in i_list:
         if ran_reg.search(i):
-            ran_start=int(i.split('-')[0])
-            ran_end=int(i.split('-')[-1])
-            for j in range(ran_start,ran_end+1):
+            ran_start = int(i.split('-')[0])
+            ran_end = int(i.split('-')[-1])
+            for j in range(ran_start, ran_end + 1):
                 str_list.append(str(j))
         else:
             str_list.append(str(i))
     return str_list
+
+
+
+
+
+class ExpectArgs(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='Login target and execute cmds')
+
+        self.parse.add_argument('-m', '--mode', required=False, default='ssh' , choices=['ssh', 'telnet'], dest='mode',
+                            help='Login mode')
+        
+        self.parse.add_argument('-i', '--ip', required=True, default=None, dest='ip',
+                            help='Target IP')
+
+        self.parse.add_argument('--port', required=False, default=22, type=int, dest='port',
+                            help='Taget port')
+
+        self.parse.add_argument('-u', '--user', required=False, default='admin', dest='user',
+                            help='Login Name')
+        
+        self.parse.add_argument('-p', '--passwd', required=False, default='aerohive', dest='passwd',
+                            help='Login Password')
+
+        self.parse.add_argument('--prompt', required=False, default='AH.*#', dest='prompt',
+                            help='The login prompt you want to meet')
+        
+        self.parse.add_argument('-t', '--timeout', required=False, default=10, type=int, dest='timeout',
+                            help='Time out value for every execute cli step')
+        
+        self.parse.add_argument('-l', '--logfile', required=False, default='.', dest='log_file',
+                            help='The log file path')
+        
+        self.parse.add_argument('-c', '--command', required=False, action='append', default=[], dest='cli_list',
+                            help='The command you want to execute')
+
+        self.parse.add_argument('-f', '--file', required=False, default=False, dest='config_file',
+                            help='The path of configurefile')
+
+        self.parse.add_argument('-w', '--wait', required=False, default=0, type=int, dest='wait',
+                            help='wait time between the current cli and next cli')
+
+        self.parse.add_argument('-r', '--retry', required=False, default=5, type=int, dest='retry',
+                            help='How many times you want to retry when the login step is failed')
+
+        self.parse.add_argument('-sp', '--shellpasswd', required=False, default='', dest='sp',
+                            help='Shell password for enter to shell mode')
+
+        self.parse.add_argument('--debug', required=False, default='error', choices=['debug', 'info', 'warn', 'error'], dest='debug_level',
+                            help='Debug mode, info>warn>error')
+
+        self._parse_args()
+        
+    def _parse_args(self):
+        self.args = self.parser.parse_args()
+        self.mode = self.args.mode
+        self.ip = self.args.ip
+        self.port = self.args.port
+        self.user = self.args.user
+        self.passwd = self.args.passwd
+        self.prompt = self.args.prompt
+        self.timeout = self.args.timeout
+        self.log_file = self.args.log_file
+        self.cli_list = self.args.cli_list
+        self.config_file = self.args.config_file
+        self.wait = self.args.wait
+        self.retry = self.args.retry
+        self.sp = self.args.sp
+        self.debug_level = self.args.debug_level
