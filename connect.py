407013a3906adafcf954a281abac7ac79c3e9fb5
willcai1984 <willcai1984@gmail.com>
2014-11-20 18:23:39 +0800

    init version for test

--- connect.py
+++ connect.py
@@ -4,15 +4,18 @@
 import sys
 from AerohiveExpect import ExpectConnect
 
-def telnet_exec():
-    telnet = ExpectConnect()
-    telnet.telnet_login()
-    telnet._basic_exec()
-    telnet._basic_logout()
+def connect_exec():
+    connect = ExpectConnect()
+    if connect.mode=='ssh':
+        connect.ssh_login()
+    elif connect.mode=='telnet':
+        connect.telnet_login()
+    connect._basic_exec()
+    connect._basic_logout()
 
 if __name__ == '__main__':
     try:
-        telnet_exec()
+        connect_exec()
     except Exception, e:
         print str(e)
         sys.exit(1)
